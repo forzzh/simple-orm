017c53975ad082c6dac96766336408aa5ffef8d1
forzzh <83986976@qq.com>
2016-10-09 14:45:48 +0800

    增加基础类

---
+++ src/com/orm/utils/JDBCUtils.java
@@ -0,0 +1,11 @@
+package com.orm.utils;
+
+/**
+ * jdbc常用操作
+ * @author flutterfire
+ * @date	2016年10月8日
+ *
+ */
+public class JDBCUtils {
+
+}
---
+++ src/com/orm/utils/ReflectUtils.java
@@ -0,0 +1,11 @@
+package com.orm.utils;
+
+/**
+ * 反射操作类
+ * @author flutterfire
+ * @date	2016年10月8日
+ *
+ */
+public class ReflectUtils {
+
+}
